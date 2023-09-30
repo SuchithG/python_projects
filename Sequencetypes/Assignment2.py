bf518cd2c71703eb338664122a5b1cbc377e47b5
SuchithG <suchithgowda1995@gmail.com>
2023-09-30 19:34:47 +0530

    Assignment2 - Datatypes

---
+++ Sequencetypes/Assignment2.py
@@ -0,0 +1,14 @@
+"""
+Define the variable names for each of the values and print them out
+10
+20.54
+True
+"I am the best"
+"""
+
+timeTaken = 10
+totalAllotedTime = 20.54
+totalTimeTaken = True
+quote = "I am the best"
+print (f"{timeTaken < totalAllotedTime}: {quote}")
+
