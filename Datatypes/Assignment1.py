2003c98469c13a736196bea9a68ff5f60b31c8c0
SuchithG <suchithgowda1995@gmail.com>
2023-09-30 18:42:39 +0530

    Assignment 1 - Datatypes

---
+++ Datatypes/Assignment1.py
@@ -0,0 +1,13 @@
+'''
+Drivers license Application useCase
+Define the fields firstName, lastName, age, ssn, height and weight using the correct Datatypes and print them
+'''
+
+firstName = "Suchith"
+lastName = "G"
+age = 28
+ssn = "123-12-1234"
+height = 178
+weight = 78.5
+
+print (f"firstName-{firstName}, lastName-{lastName}, age-{age}yrs, ssn-{ssn}, height-{height}cms, weight-{weight}kgs")
