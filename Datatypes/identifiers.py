e18b59a54880004870bbc3642546673fc548a740
SuchithG <suchithgowda1995@gmail.com>
2023-09-30 18:20:16 +0530

    Datatypes

---
+++ Datatypes/identifiers.py
@@ -0,0 +1,12 @@
+#Identifiers
+
+userID = 123
+userId = 123
+user_ID = 123
+user_id_ = 123
+_userID = 123 #private
+__userID = 123 #More private
+__init__ = 123 # if their is two "__" at the begining and end of a function - special/magic functions
+# user_ID$ = 123 # Cannot have any special characters 
+# if = 123 # cannot have any in-built keywords
+# 23userID = 123 # cannot have number at beginning of function
