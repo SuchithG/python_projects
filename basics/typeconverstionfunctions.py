e93a2029253d12060f0138e1d534041a538c2776
SuchithG <suchithgowda1995@gmail.com>
2023-09-30 18:06:17 +0530

    type conversion functions with example

---
+++ basics/typeconverstionfunctions.py
@@ -0,0 +1,13 @@
+#Type Conversion functions
+x=35.2
+h = int(x)  #float to int
+print(h)
+print(type(h))
+
+i = float('22.5') #string to float
+print(i)
+print(type(i))
+
+print(bin(11)) # binary conversion
+print(hex(11)) # hexadecimal conversion
+print(oct(11)) # octadecimal conversion
