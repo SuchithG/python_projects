22951a75a32653b05d983f9eb8a5ba2bb946806c
SuchithG <suchithgowda1995@gmail.com>
2023-09-30 19:00:03 +0530

    Defining a string, Indexing, Repetition len function, Slicing,

---
+++ Sequencetypes/stringtype.py
@@ -0,0 +1,28 @@
+# single line strings
+s = "you are awesome"
+print(s)
+
+
+# Multi-line strings
+s1 = """You are the creator 
+of you 
+own destiny"""
+print(s1)
+
+print(s[4]) #Indexing
+
+print(s*2) #Repetition
+
+#Length function
+print(len(s))
+print(len(s1))
+
+#Slicing
+print(s[0:3]) # does not include the element in the ending index
+print(s[0:])
+print(s[:5])
+print(s[-3:-1])
+
+print(s[0:9:2]) # adding a step while indexing
+print(s[10::-1])
+print(s[::-1]) # starting index from the end
