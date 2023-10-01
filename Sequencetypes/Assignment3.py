4eb4d676e533fb23e785de7dae392f453a86181a
SuchithG <suchithgowda1995@gmail.com>
2023-10-01 17:14:12 +0530

    Assignment 3

---
+++ Sequencetypes/Assignment3.py
@@ -0,0 +1,18 @@
+"""
+Collection Assignment - Initialize a list of countries with three countries in the list
+- add an country at the end of the list
+- remove an country using index
+- add a county in the middle of the list 
+"""
+
+country_list = ["India", "Pakistan", "Canada"]
+
+country_list.append("Brazil")
+
+del country_list[1]
+
+country_list = set(country_list)
+
+country_list.update(["France"])
+
+print(country_list)
