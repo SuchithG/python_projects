5fa6d8011c896a5e59359f52f6e52e1e9e8b375a
SuchithG <suchithgowda1995@gmail.com>
2023-10-01 14:28:26 +0530

    More functions

--- Sequencetypes/listtype.py
+++ Sequencetypes/listtype.py
@@ -6,10 +6,20 @@ print(lst*2) # Repeating
 print(len(lst)) # Length
 
 # Dynamically adding or removing elements from a list\
-lst.append(40)
+lst.append(40) # adds elements to the list at the end 
 lst.remove("Suchith")
 del(lst[3]) # del function to remove elements using index
 print(lst)
 
-lst.clear() # Remove all elements from list
+#lst.clear() # Remove all elements from list
+#print(lst)
+
+print(max(lst)) # print max element
+print(min(lst)) # print min element
+
+lst.insert(3, 99) # insert element at the index
+print(lst)
+
+lst.sort() # sort elements in ascending
+lst.reverse() # reverse elements
 print(lst)
