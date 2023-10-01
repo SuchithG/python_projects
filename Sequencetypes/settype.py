059bf3d64dbf3f7d6c4f2625b5f3c7eef5c0843b
SuchithG <suchithgowda1995@gmail.com>
2023-10-01 15:09:56 +0530

    set types

---
+++ Sequencetypes/settype.py
@@ -0,0 +1,13 @@
+s = {23, 56, -79, "OK", "OK"} # does not consider duplicate values
+print(s)
+
+s.update([88,99]) # does not update values in particular order
+print(s)
+
+# print(s*2) # does not allow repeating 
+
+s.remove(-79) # removes
+print(s)
+
+
+
