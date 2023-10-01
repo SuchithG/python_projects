9beef72249fade18fabce8303158070eb2318fbc
SuchithG <suchithgowda1995@gmail.com>
2023-10-01 17:00:42 +0530

    Immutability

---
+++ Sequencetypes/immutable.py
@@ -0,0 +1,17 @@
+a=20
+b=20
+print (id(a)) # prints the id of the object
+print(id(b))
+print(a is b)
+
+a=True
+b=False
+print (id(a)) # prints the id of the object
+print(id(b))
+print(a is b) 
+
+a="suchith"
+b=" suchith"
+print (id(a)) # prints the id of the object
+print(id(b))
+print(a is b) 
