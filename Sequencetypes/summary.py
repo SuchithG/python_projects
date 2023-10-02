18f5b97645920ffb6d278596f91c6d942cdcddda
SuchithG <suchithgowda1995@gmail.com>
2023-10-02 22:46:24 +0530

    revision

---
+++ Sequencetypes/summary.py
@@ -0,0 +1,10 @@
+a = 20
+b = 20
+b=30
+print(id(a), id(b))
+
+f = 3.14
+c = 5+3j
+print(type(c))
+print(c.real) # to print the real part of complex types
+print(c.imag) # to print the imag part of complex types
