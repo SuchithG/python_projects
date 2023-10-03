5aebae3f81f52075468faba3d61f541c868ee8b0
SuchithG <suchithgowda1995@gmail.com>
2023-10-03 20:41:17 +0530

    arithmetic operators

---
+++ operators/arithmeticoperators.py
@@ -0,0 +1,9 @@
+a,b = 10,5
+
+print("addition",a+b) # add 
+print("subtraction",a-b) # subtract 
+print("Mul",a*b) # multiply
+print("Div",a/b) # divide 
+print("Mod",a%b) # remainder
+print("Pow",a**b) # power of
+print("Floor div",a//b) # right value if quotient after division
