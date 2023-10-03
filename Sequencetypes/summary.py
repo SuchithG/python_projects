646046ed4415b0e3c102ce4b9542aa1ef5c58d41
SuchithG <suchithgowda1995@gmail.com>
2023-10-03 20:13:27 +0530

    revision

--- Sequencetypes/summary.py
+++ Sequencetypes/summary.py
@@ -8,3 +8,11 @@ c = 5+3j
 print(type(c))
 print(c.real) # to print the real part of complex types
 print(c.imag) # to print the imag part of complex types
+
+s="Suchith"
+print(type(s))
+lst = [1,2,3,4,5,6,7,8,9,10]
+print(type(lst))
+b = bytes(lst)
+print(type(b))
+
