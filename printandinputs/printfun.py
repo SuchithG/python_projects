d2684b52466ee47029380fef60a0aaaac05855ad
SuchithG <suchithgowda1995@gmail.com>
2023-10-09 22:45:39 +0530

    separators

--- printandinputs/printfun.py
+++ printandinputs/printfun.py
@@ -3,3 +3,6 @@ print() # Empty print statement
 print("Hello"*3) # prints Hello three times
 
 print("All the power \n is within you!") # new line characters
+
+a,b = 10,20
+print(a,b, sep="+") # separator
