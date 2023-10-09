632531ca4429b8716afa290b41f70b7569cf26b6
SuchithG <suchithgowda1995@gmail.com>
2023-10-09 22:44:29 +0530

    new line character

--- printandinputs/printfun.py
+++ printandinputs/printfun.py
@@ -2,4 +2,4 @@ print() # Empty print statement
 
 print("Hello"*3) # prints Hello three times
 
-print("All the power \n is within you!") # new line print
+print("All the power \n is within you!") # new line characters
