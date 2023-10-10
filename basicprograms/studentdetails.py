b8f3dfe0c5d7d170f8d5d4ad7c5cc1905ddf113c
SuchithG <suchithgowda1995@gmail.com>
2023-10-10 23:30:45 +0530

    Read and Display student details

---
+++ basicprograms/studentdetails.py
@@ -0,0 +1,5 @@
+student_id = int(input("Enter the student ID "))
+name = input("Enter the name ")
+marks = float(input("Enter the marks scored "))
+
+print("ID:", student_id, "Name:", name,"marks:", marks)
