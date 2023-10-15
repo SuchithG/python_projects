e67eab0e57786eb07fd5c73eb6163c3547da1534
SuchithG <suchithgowda1995@gmail.com>
2023-10-15 12:21:28 +0530

    removing duplicates using set

--- controlstatements/removeduplicates.py
+++ controlstatements/removeduplicates.py
@@ -1,6 +1,7 @@
 # Remove the duplicates in list
 
 l1 = eval(input("Enter a list of elements: "))
+'''
 print(l1)
 
 l2=[]
@@ -8,4 +9,7 @@ l2=[]
 for each_value in l1:
     if each_value not in l2:
         l2.append(each_value)
+print(l2)
+'''
+l2 = set(l1)
 print(l2)
