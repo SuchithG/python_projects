5831cacf9aa1b367fd27f0693f55342a86a1290b
SuchithG <suchithgowda1995@gmail.com>
2023-10-15 12:42:11 +0530

    sorting alphabetically

--- controlstatements/vowelcounter.py
+++ controlstatements/vowelcounter.py
@@ -7,5 +7,5 @@ result = {}
 for c in word:
     if c in vowels:
         result[c] = result.get(c, 0) + 1
-for k, v in result.items():
+for k, v in sorted(result.items()):
     print(k, " is present", v, " times")
