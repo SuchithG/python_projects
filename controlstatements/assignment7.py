3d2f59bcde225669c1dd4a105f1c898094feade5
SuchithG <suchithgowda1995@gmail.com>
2023-10-16 22:11:17 +0530

    .

--- controlstatements/assignment7.py
+++ controlstatements/assignment7.py
@@ -1,4 +1,5 @@
 # Enter a number and determine wether it is prime or not.
+#uses a flag variable to keep track of whether the number is prime and a while loop to check for divisibility by all numbers from 2 up to num - 1. If any of these divisions result in a remainder of 0, the flag is set to False, indicating that the number is not prime. Otherwise, if no divisions result in a remainder of 0, the flag remains True, indicating that the number is prime.
 
 flag = True
 i = 2
