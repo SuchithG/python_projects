f7ca30b3be45b3de64cc92873c01b9829aab7ef2
SuchithG <suchithgowda1995@gmail.com>
2023-10-16 21:39:42 +0530

    eg

--- controlstatements/employeeuinfo.py
+++ controlstatements/employeeuinfo.py
@@ -1,1 +1,21 @@
 # Enter the employee details and input the employee name to retrieve the employee salary
+
+n = int(input('Enter the number of employees'))
+
+employees = {}
+
+for i in range(n):
+    name = input('Enter employee name')
+    salary = input('Enter employee salary')
+    employees[name] = salary
+
+while True:
+    name = input('Enter employee name')
+    salary = employees.get(name, -1)
+    if salary == -1:
+        print("Employee does not exist")
+    else:
+        print("The salary of the employee is", salary)
+    choice = input('Do you want to know the salary of another employee[Yes|No]?')
+    if choice == 'No':
+        break
