c6a838ea38ea1d8d4932a2a11673c44f5060326c
SuchithG <suchithgowda1995@gmail.com>
2023-10-16 21:57:20 +0530

    assignment

--- controlstatements/assignment6.py
+++ controlstatements/assignment6.py
@@ -3,3 +3,14 @@ Ask the user to enter a number and display that number
 skip the if the number is divisible by 10 
 stop if number > 100
 '''
+
+n = int(input("Enter a number: "))
+x=0
+while x < 100:
+    x+= 1
+    if x%10 == 0:
+        continue
+    if (x==100):
+        break
+    print(x)
+    
