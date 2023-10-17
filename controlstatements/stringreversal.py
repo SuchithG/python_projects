614fcd80b97676894f466e7a18f4abaa885cba40
SuchithG <suchithgowda1995@gmail.com>
2023-10-17 15:53:51 +0530

    eg-1 for checking palindrome

--- controlstatements/stringreversal.py
+++ controlstatements/stringreversal.py
@@ -1,4 +1,13 @@
 # Ask the end user to enter a string and then return the reversed string
-
+'''
 s = input("Enter a string: ")
 print(s[::-1])
+'''
+
+s = input("Enter a string: ")
+i = len(s) - 1
+result = ''
+while i >= 0:
+    result = result + s[i]
+    i = i-1
+print(result)
