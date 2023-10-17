900a7fbcdd75ed295d0d2fac1167b8a3c99b46cc
SuchithG <suchithgowda1995@gmail.com>
2023-10-17 16:07:34 +0530

    eg-1

---
+++ controlstatements/wordreversal.py
@@ -0,0 +1,13 @@
+# Reverse the words in a string
+
+s = 'All the power is with in you'
+temp = s.split()
+print(temp)
+result = []
+i = len(temp)-1
+while i > 0:
+    result.append(temp[i])
+    i = i-1
+print(result)
+output = ' '.join(result)
+print(output)
