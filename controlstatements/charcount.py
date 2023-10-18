72abf541456780e437fcbbc96f07b31848627543
SuchithG <suchithgowda1995@gmail.com>
2023-10-18 21:59:26 +0530

    eg

--- controlstatements/charcount.py
+++ controlstatements/charcount.py
@@ -1,1 +1,12 @@
 # Count the characters in a string
+
+s = 'aaahhnshdvsadvasdvdvasdgvasv'
+
+d = {}
+for c in s:
+    if c in d.keys():
+        d[c] = d[c]+1
+    else:
+        d[c] = 1
+for k,v in d.items():
+    print('{}={} times'.format(k,v))
