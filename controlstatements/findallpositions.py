b5f6fd09b3282a3945e297265cdd0c70074637ec
SuchithG <suchithgowda1995@gmail.com>
2023-10-18 22:25:48 +0530

    eg-1

--- controlstatements/findallpositions.py
+++ controlstatements/findallpositions.py
@@ -1,1 +1,16 @@
 # Given a string find all positions of substrings 
+
+s = "Take up one idea and make that idea your life."\
+    "Think and dream of that idea and leave every idea alone."
+subs = "idea"
+found = False
+pos = -1
+length = len(s)
+while True:
+    pos = s.find(subs, pos+1, length)
+    if pos == -1:
+        break
+    print("Found the sub string at position", pos)
+    found = True
+if found == False:
+    print("Sub string not found")
