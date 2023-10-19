77a20fc02f9fa45e0d4dc42d703fc52a8de758c6
SuchithG <suchithgowda1995@gmail.com>
2023-10-19 14:14:39 +0530

    command line arguments - display

---
+++ commandlinearguments/demo.py
@@ -0,0 +1,10 @@
+# Display the command line arguments
+
+import sys
+
+lst=sys.argv  # getting the list using sys.argv
+for i in lst:print(i)
+
+print(len(lst))  # Display the command line arguments length
+
+print(lst[0]) # Display the script/Program name
