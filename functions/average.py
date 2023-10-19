387337140b7b2442cf1b3cd4307802ad0afd36bc
SuchithG <suchithgowda1995@gmail.com>
2023-10-19 15:24:13 +0530

    returning a result

--- functions/average.py
+++ functions/average.py
@@ -1,6 +1,8 @@
 # function to calculate the average of two numbers 
 
 def average(a, b):
-    print("Average of two numbers is",(a+b)/2)
+    #print("Average of two numbers is",(a+b)/2)
+    return (a+b)/2
 
-average(10, 20)
+
+print(average(10, 20))
