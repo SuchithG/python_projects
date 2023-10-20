ae55d441a41f3666a1d4c2cf1087b87034d182b2
SuchithG <suchithgowda1995@gmail.com>
2023-10-20 17:49:44 +0530

    using keyword arguments

--- functions/average.py
+++ functions/average.py
@@ -1,8 +1,10 @@
 # function to calculate the average of two numbers 
 
 def average(a, b):
+    print(a)
+    print(b)
     #print("Average of two numbers is",(a+b)/2)
     return (a+b)/2
 
 
-print(average(10, 20))
+print(average(b=10, a=20)) # passing keyword argument
