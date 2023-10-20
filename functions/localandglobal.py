5d79b1a90e6cf076b845a0c78623904d7cca0ddb
SuchithG <suchithgowda1995@gmail.com>
2023-10-20 17:02:44 +0530

    accessing global variables with the same name

--- functions/localandglobal.py
+++ functions/localandglobal.py
@@ -1,10 +1,12 @@
 # local and global variables
+# accessing global variables with the same name
 
 x = 123
 
 def display():
     y = 678
     print(y)
+    print(globals()['x']) 
 
 print(x)
 display()
