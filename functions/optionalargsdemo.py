0dc354033a0101c71a8bbbe5c2e0ea1dbd2d8f99
SuchithG <suchithgowda1995@gmail.com>
2023-10-21 19:21:44 +0530

    acess using for loop

--- functions/optionalargsdemo.py
+++ functions/optionalargsdemo.py
@@ -2,7 +2,7 @@
 
 def myfun(x, *args, **kwargs):
     print(x)
-    print(args)
-    print(kwargs)
+    for each_args in args:
+        print(each_args)
 
 myfun(10,20,30,40,50, name="Suchith", sal="10000000")
