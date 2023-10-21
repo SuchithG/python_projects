2e32a5dc757caef0f23449788c29ac13b5b788ac
SuchithG <suchithgowda1995@gmail.com>
2023-10-21 19:22:26 +0530

    access keyword using for loop

--- functions/optionalargsdemo.py
+++ functions/optionalargsdemo.py
@@ -4,5 +4,7 @@ def myfun(x, *args, **kwargs):
     print(x)
     for each_args in args:
         print(each_args)
+    for each_kw in kwargs:
+        print(each_kw)
 
 myfun(10,20,30,40,50, name="Suchith", sal="10000000")
