6e1fa89a83d7585e7856ef3fd213e4626e67322c
SuchithG <suchithgowda1995@gmail.com>
2023-10-21 19:19:45 +0530

    keyword args eg

--- functions/optionalargsdemo.py
+++ functions/optionalargsdemo.py
@@ -1,7 +1,8 @@
 # variable args and optional arguments
 
-def myfun(x, *args):
+def myfun(x, *args, **kwargs):
     print(x)
     print(args)
+    print(kwargs)
 
-myfun(10,20,30,40,50)
+myfun(10,20,30,40,50, name="Suchith", sal="10000000")
