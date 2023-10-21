fa5e58570b9c27562301716ee1a6225ca77b8e0f
SuchithG <suchithgowda1995@gmail.com>
2023-10-21 19:26:13 +0530

    using custom names

--- functions/optionalargsdemo.py
+++ functions/optionalargsdemo.py
@@ -1,9 +1,9 @@
 # variable args and optional arguments
 
-def myfun(x, *args, **kwargs):
+def myfun(x, *pos_param, **key_param):
     print(x)
-    print(args)
-    for key, value in kwargs.items():
+    print(pos_param)
+    for key, value in key_param.items():
         print(key, value)
 
 myfun(10,20,30,40,50, name="Suchith", sal="10000000")
