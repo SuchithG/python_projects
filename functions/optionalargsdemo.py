8757fa3cc8eab8acb24b7d747e9622d087485e86
SuchithG <suchithgowda1995@gmail.com>
2023-10-21 19:24:28 +0530

    key value example

--- functions/optionalargsdemo.py
+++ functions/optionalargsdemo.py
@@ -2,9 +2,8 @@
 
 def myfun(x, *args, **kwargs):
     print(x)
-    for each_args in args:
-        print(each_args)
-    for each_kw in kwargs:
-        print(each_kw)
+    print(args)
+    for key, value in kwargs.items():
+        print(key, value)
 
 myfun(10,20,30,40,50, name="Suchith", sal="10000000")
