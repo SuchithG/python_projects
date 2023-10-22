157adb4e5da144a040b352d307dde1a5f838afae
SuchithG <suchithgowda1995@gmail.com>
2023-10-22 20:50:53 +0530

    passing optional parameters to other functions

--- functions/optionalargsdemo.py
+++ functions/optionalargsdemo.py
@@ -2,8 +2,16 @@
 
 def myfun(x, *pos_param, **key_param):
     print(x)
+    key_param['id'] = 123
     print(pos_param)
     for key, value in key_param.items():
         print(key, value)
+    modified_pos_param = pos_param+(50,)
+    myfun2(*modified_pos_param, **key_param)
+    
+
+def myfun2(*args, **kwargs):
+    print(args)
+    print(kwargs)
 
 myfun(10,20,30,40,50, name="Suchith", sal="10000000")
