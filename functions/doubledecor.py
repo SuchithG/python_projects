894fb711bdb2d85845183ff916fe5339a4466c8e
SuchithG <suchithgowda1995@gmail.com>
2023-10-23 23:33:22 +0530

    using decor function

--- functions/doubledecor.py
+++ functions/doubledecor.py
@@ -1,1 +1,13 @@
 # create a decorator function that will return double the number returned ny that function
+
+def decor(fun):
+    def inner():
+        result = fun()
+        return result*2
+    return inner
+
+def num():
+    return 5
+
+resultfun = decor(num)
+print(resultfun())
