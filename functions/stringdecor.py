d978a9e94dcd0a646789dfdcb1362b95889fb97c
SuchithG <suchithgowda1995@gmail.com>
2023-10-23 23:42:19 +0530

    Decorating strings

---
+++ functions/stringdecor.py
@@ -0,0 +1,14 @@
+# Decorating strings
+
+def decorfun(fun):
+    def inner(n):
+        result = fun(n)
+        result += "How are you?"
+        return result
+    return inner
+
+@decorfun
+def hello(name):
+    return "Hello " + name
+
+print(hello("Suchith "))
