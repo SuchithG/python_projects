aa371c406f6ef651628478664fdcea1f6571e2c5
SuchithG <suchithgowda1995@gmail.com>
2023-10-25 00:23:11 +0530

    feat:

--- functions/doubleusingmap.py
+++ functions/doubleusingmap.py
@@ -1,4 +1,5 @@
 # Using the map function double the value of each element in the list
+# applies a function/lambda on every element of the list and gives us a new list
 
 lst = [1,2,3,4,5,6,7,8,9,10]
 
