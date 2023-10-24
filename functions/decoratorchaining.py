8aabdfbbd8ba9302f83a2b4faed7e736ec233334
SuchithG <suchithgowda1995@gmail.com>
2023-10-25 00:03:20 +0530

    Decorator chaining

--- functions/decoratorchaining.py
+++ functions/decoratorchaining.py
@@ -1,9 +1,16 @@
+def square(fun):
+    def inner():
+        n = fun()
+        return n*n
+    return inner
+
 def half(fun):
     def inner():
         n = fun()
         return n/2
     return inner
 
+@square
 @half
 def num():
     return 10
