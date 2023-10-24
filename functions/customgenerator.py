e543ddd7ca9fd592c0f9251640fc14fb95ae5fd8
SuchithG <suchithgowda1995@gmail.com>
2023-10-25 00:10:39 +0530

    custom generator

---
+++ functions/customgenerator.py
@@ -0,0 +1,10 @@
+# create a generator
+def customgen(x,y):
+    while x<y:
+        yield x # will stack each value of x and store it in memory
+        x+=1
+
+result = customgen(20,30)
+
+for i in result:
+    print(i)
