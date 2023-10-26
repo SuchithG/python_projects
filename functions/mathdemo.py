189174a048eb4974c03a51b3b00d624e805da63a
SuchithG <suchithgowda1995@gmail.com>
2023-10-26 23:29:59 +0530

    list functions of the module using dir

--- functions/mathdemo.py
+++ functions/mathdemo.py
@@ -7,3 +7,5 @@ print(ceil(8.2)) # ceil of number
 print(floor(7.9)) # floor value of number
 
 print(fabs(-2.2)) # floating absolute value of number
+
+print(dir()) # returns the list of functions of the math module as well as the default functions
