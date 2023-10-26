5ecfc5189fa956368b57b3610a3272002df28771
SuchithG <suchithgowda1995@gmail.com>
2023-10-26 23:34:16 +0530

    module details - help( )

--- functions/mathdemo.py
+++ functions/mathdemo.py
@@ -1,4 +1,5 @@
 from math import *
+import math
 
 print(sqrt(9)) # square root
 
@@ -9,5 +10,7 @@ print(floor(7.9)) # floor value of number
 print(fabs(-2.2)) # floating absolute value of number
 
 print(dir()) # returns the list of functions of the math module as well as the default functions
+
+help(math) # details of the math module with location information
 
 # default - ['__annotations__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__']
