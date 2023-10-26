a7153254db4e987c2a0e0d610affa90355d64fe3
SuchithG <suchithgowda1995@gmail.com>
2023-10-26 23:31:30 +0530

    default - files

--- functions/mathdemo.py
+++ functions/mathdemo.py
@@ -9,3 +9,5 @@ print(floor(7.9)) # floor value of number
 print(fabs(-2.2)) # floating absolute value of number
 
 print(dir()) # returns the list of functions of the math module as well as the default functions
+
+# default - ['__annotations__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__']
