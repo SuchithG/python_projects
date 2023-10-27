a4ed8fd1a1f147f59de72e3ce658d037bb6b8848
SuchithG <suchithgowda1995@gmail.com>
2023-10-27 11:21:39 +0530

    random numbers - randint

--- functions/randomdemo.py
+++ functions/randomdemo.py
@@ -2,3 +2,6 @@ from random import *
 
 for i in range(10): # for range 10 random numbers
     print(random()) # will return a random floating point number between 0 and 1
+
+for i in range(10): # for range 10 random numbers
+    print(randint(1,50)) # random numbers between 1 to 50
