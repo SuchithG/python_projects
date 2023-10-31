72bcb8c86b2d0bd0ab6a94187e054a9027958d2c
SuchithG <suchithgowda1995@gmail.com>
2023-10-31 19:23:42 +0530

    defining and class and creating object

---
+++ oopsbasics/product.py
@@ -0,0 +1,12 @@
+class product:
+
+    def __init__(self):
+        self.name = 'IPhone'
+        self.description = 'Its Awesome'
+        self.price = 700
+
+p1 = product()
+
+print(p1.name)
+print(p1.description)
+print(p1.price)
