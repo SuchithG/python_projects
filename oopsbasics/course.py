c17c6857b024da34898601d05808ac2d1f12655e
SuchithG <suchithgowda1995@gmail.com>
2023-10-31 19:38:54 +0530

    Parameterized Constructors

--- oopsbasics/course.py
+++ oopsbasics/course.py
@@ -1,1 +1,11 @@
 # Parameterized Constructors
+
+class Course:
+    
+    def __init__(self,name,ratings):
+        self.name = name
+        self.ratings = ratings
+
+c1 = Course("Java Course", [1,2,3,4,5])
+print(c1.name)
+print(c1.ratings)
