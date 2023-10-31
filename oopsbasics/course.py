a3a6ec41cf2765b024504ea2d6faaed6e80e42f1
SuchithG <suchithgowda1995@gmail.com>
2023-10-31 19:54:06 +0530

    Defined an instance method

--- oopsbasics/course.py
+++ oopsbasics/course.py
@@ -6,9 +6,15 @@ class Course:
         self.name = name
         self.ratings = ratings
 
+    def average(self):
+        numberOfRatings = len(self.ratings)
+        average = sum(self.ratings)/numberOfRatings
+        print("Average Ratings for", self.name, "Is", average)
+
 c1 = Course("Java Course", [1,2,3,4,5])
 print(c1.name)
 print(c1.ratings)
+c1.average()
 
 c2 = Course("Java web services", [5,5,5,5,5])
 print(c2.name)
