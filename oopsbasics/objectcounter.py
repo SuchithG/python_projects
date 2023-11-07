d4a9bffe2ad0415c625945a8e77d7448c8e834d6
SuchithG <suchithgowda1995@gmail.com>
2023-11-07 23:20:10 +0530

    Creating static field and method

---
+++ oopsbasics/objectcounter.py
@@ -0,0 +1,17 @@
+# Define a static field and method
+
+class ObjectCounter:
+
+    numberOfObjects = 0
+
+    def __init__(self):
+        ObjectCounter.numberOfObjects+= 1
+
+    @staticmethod
+    def displayCount():
+        print(ObjectCounter.numberOfObjects)
+
+o1 = ObjectCounter()
+o2 = ObjectCounter()
+
+ObjectCounter.displayCount()
