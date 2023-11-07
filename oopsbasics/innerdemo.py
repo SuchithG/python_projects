2dcaf5b648074918106aab9e908cc128c0c7b3bb
SuchithG <suchithgowda1995@gmail.com>
2023-11-07 23:23:33 +0530

    inner class

--- oopsbasics/innerdemo.py
+++ oopsbasics/innerdemo.py
@@ -4,3 +4,7 @@ class Car:
     def __init__(self, make, year):
         self.make = make
         self.year = year
+
+    class Engine:
+        def __init__(self, number):
+            self.number = number
