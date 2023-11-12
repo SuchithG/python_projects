a19191d98314535dcbca96df108d663e9eae48b9
SuchithG <suchithgowda1995@gmail.com>
2023-11-12 21:16:46 +0530

    fix: :bug:

--- inheritance/bmw.py
+++ inheritance/bmw.py
@@ -14,16 +14,16 @@ class BMW:
 class ThreeSeries(BMW):
 
     def __init__(self, curiseControlEnabled, make, model, year):
-        super().__init__(make, model, year)
+        BMW.__init__(self, make, model, year)
         self.curiseControlEnabled = curiseControlEnabled
 
     def display(self):
         print(self.curiseControlEnabled)
 
-class FiveSeries:
+class FiveSeries(BMW):
 
     def __init__(self, parkingAssistEnabled, make, model, year):
-        super().__init__(self, make, model, year)
+        BMW.__init__(self, make, model, year)
         self.parkingAssistEnabled = parkingAssistEnabled
 
 threeSeries = ThreeSeries(True, "BMW", "328i", "2018")
