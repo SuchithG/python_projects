394def35ace0e3ae0350711ed1ed77bd702b7804
SuchithG <suchithgowda1995@gmail.com>
2023-11-21 23:08:25 +0530

    Create and Raise custom exception

--- exceptionHandling/customexception.py
+++ exceptionHandling/customexception.py
@@ -1,1 +1,11 @@
 # Create and Raise Custom Exceptions
+
+class overTheLimitException(Exception):
+    def __init__(self, msg):
+        self.msg = msg # adding msg to the constructor
+
+def withdrawl(amount):
+    if(amount > 500):
+        raise overTheLimitException("You cannot withdraw more than 500$ per day")
+    
+withdrawl(600)
