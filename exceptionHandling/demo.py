01b97ccdd754cd80adbbe712b1bebc045d8263fe
SuchithG <suchithgowda1995@gmail.com>
2023-11-21 22:24:19 +0530

    using finally

--- exceptionHandling/demo.py
+++ exceptionHandling/demo.py
@@ -1,10 +1,14 @@
 # ZeroDivisionError : division by zero
 
 try:
+    f = open("myfile", "w")
     a, b = [int(x) for x in input("Enter two numbers:").split()]
     c = a/b
-    print(c)
+    f.write("Writing %d to into file" %c)
 except ZeroDivisionError:
     print("Division by zero is not allowed")
     print("Please enter a non-zero number")
+finally:
+    f.close()
+    print("File closed")
 print("Code after the exception")
