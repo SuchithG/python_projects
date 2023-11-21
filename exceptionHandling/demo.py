fb1338fc72e6ee4fb93cfc9bc3e98d3c696c018b
SuchithG <suchithgowda1995@gmail.com>
2023-11-21 22:25:09 +0530

    using Else

--- exceptionHandling/demo.py
+++ exceptionHandling/demo.py
@@ -8,6 +8,8 @@ try:
 except ZeroDivisionError:
     print("Division by zero is not allowed")
     print("Please enter a non-zero number")
+else:
+    print("You have entered a non-zero number")
 finally:
     f.close()
     print("File closed")
