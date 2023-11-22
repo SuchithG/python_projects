9834daa975964af78eff344014f62882bcaffee9
SuchithG <suchithgowda1995@gmail.com>
2023-11-22 21:40:00 +0530

    check if the file exists

--- files/read.py
+++ files/read.py
@@ -1,6 +1,12 @@
 # read from an existing file
+import os, sys
 
-f = open("myfile.txt", "r")
+if os.path.isfile('myfile.txt'):
+    f = open('myfile.txt', 'r')
+else: 
+    print("File Does not Exist")
+    sys.exit(1)
+
 s = f.read()
 print(s)
 f.close()
