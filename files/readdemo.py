5f7e13d9d5e3830331ac2bb17f9e442feb3290e8
SuchithG <suchithgowda1995@gmail.com>
2023-11-24 10:15:19 +0530

    readlines

--- files/readdemo.py
+++ files/readdemo.py
@@ -2,5 +2,9 @@ f = open('sample.txt', 'r')
 
 print(f.read())
 
-f.seek(0)
+f.seek(0) # file cursor to beginning or any position
 print(f.readline())
+
+f.seek(0)
+print(f.readlines()) # returns the list of all the lines in the file from beginning along with the new line character
+f.close()
