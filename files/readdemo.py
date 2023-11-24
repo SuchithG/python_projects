3fdaa439878126858ef5fe9b444adae346c199f1
SuchithG <suchithgowda1995@gmail.com>
2023-11-24 10:16:37 +0530

    readline 3 characters

--- files/readdemo.py
+++ files/readdemo.py
@@ -3,7 +3,7 @@ f = open('sample.txt', 'r')
 print(f.read(12))
 
 f.seek(0) # file cursor to beginning or any position
-print(f.readline())
+print(f.readline(3))
 
 f.seek(0)
 print(f.readlines()) # returns the list of all the lines in the file from beginning along with the new line character
