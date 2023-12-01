9b9725a1e7c4b2a3bea320e067040ce91626e737
SuchithG <suchithgowda1995@gmail.com>
2023-12-01 11:31:16 +0530

    cursor location

--- files/writedemo.py
+++ files/writedemo.py
@@ -2,5 +2,7 @@ f = open('sample.txt', 'w+')
 
 f.write("Python is awesome\n")
 f.writelines(["Python\n","Django\n","DRF\n"]) # writes a list in new line in list
+print('Cursor is at ', f.tell())
 f.seek(0) # cursor will move to the beginning of the file
+print('Cursor is at ', f.tell())
 print(f.read())  # Read the contents of the file
