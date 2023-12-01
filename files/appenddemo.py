b169312ceefc55ca4b042b46c08849b03e5d4e08
SuchithG <suchithgowda1995@gmail.com>
2023-12-01 11:38:01 +0530

    append into a list a

--- files/appenddemo.py
+++ files/appenddemo.py
@@ -1,2 +1,9 @@
 f = open('sample.txt', 'a+') # append mode
 print('Cursor at', f.tell())  # for append the cursor position is at the end 
+print("Django is for web development\n") # new line being appended to an existing file
+f.seek(0)
+a = []
+for line in f:
+    a.append(line)
+print(a)
+f.close()
