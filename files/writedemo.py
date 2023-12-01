3b9fb8cd490260ed1ab015bcef240912f5b4c923
SuchithG <suchithgowda1995@gmail.com>
2023-12-01 11:28:54 +0530

    seek() & read()

--- files/writedemo.py
+++ files/writedemo.py
@@ -2,3 +2,5 @@ f = open('sample.txt', 'w+')
 
 f.write("Python is awesome\n")
 f.writelines(["Python\n","Django\n","DRF\n"]) # writes a list in new line in list
+f.seek(0) # cursor will move to the beginning of the file
+print(f.read())  # Read the contents of the file
