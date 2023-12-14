9a868cafa77b4431c958b1f24ec236cd91b20a8a
SuchithG <suchithgowda1995@gmail.com>
2023-12-14 18:04:43 +0530

    using quantifiers

--- regularexpressions/redemo.py
+++ regularexpressions/redemo.py
@@ -1,8 +1,9 @@
 import re 
 
-str = "Take 1 up one 23 idea.one idea 45 at a time"
+str = "Take 1 up One 23 idea.One idea 45 at a time"
+
 result = re.search(r'o\w\w',str)
-print(result.group())
+#print(result.group())
 
 result = re.findall(r'o\w\w',str)
 print(result)
@@ -17,5 +18,8 @@ result = re.findall(r'o\w{1,2}',str)
 print(result)
 
 result = re.split(r'\d+',str)
+print(result)
+
+result = re.findall(r'O\w+',str) # print one or more characters, starting with the preceding alphabet
 print(result)
 
